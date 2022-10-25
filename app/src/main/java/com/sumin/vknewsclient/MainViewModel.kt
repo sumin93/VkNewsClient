88b381614081881c4101ee779ae0ef3a892e3159
a.sumin <sumin93@mail.ru>
2022-10-26 01:20:29 +0300

    Add HomeScreenState.kt

--- app/src/main/java/com/sumin/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sumin.vknewsclient.domain.FeedPost
 import com.sumin.vknewsclient.domain.StatisticItem
+import com.sumin.vknewsclient.ui.theme.HomeScreenState
 
 class MainViewModel : ViewModel() {
 
@@ -13,11 +14,13 @@ class MainViewModel : ViewModel() {
             add(FeedPost(id = it))
         }
     }
-    private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
-    val feedPosts: LiveData<List<FeedPost>> = _feedPosts
+    private val initialState = HomeScreenState.Posts(posts = sourceList)
+
+    private val _screenState = MutableLiveData<HomeScreenState>(initialState)
+    val screenState: LiveData<HomeScreenState> = _screenState
 
     fun updateCount(feedPost: FeedPost, item: StatisticItem) {
-        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldPosts = screenState.value?.toMutableList() ?: mutableListOf()
         val oldStatistics = feedPost.statistics
         val newStatistics = oldStatistics.toMutableList().apply {
             replaceAll { oldItem ->
@@ -29,7 +32,7 @@ class MainViewModel : ViewModel() {
             }
         }
         val newFeedPost = feedPost.copy(statistics = newStatistics)
-        _feedPosts.value = oldPosts.apply {
+        _screenState.value = oldPosts.apply {
             replaceAll {
                 if (it.id == newFeedPost.id) {
                     newFeedPost
@@ -41,9 +44,9 @@ class MainViewModel : ViewModel() {
     }
 
     fun remove(feedPost: FeedPost) {
-        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldPosts = screenState.value?.toMutableList() ?: mutableListOf()
         oldPosts.remove(feedPost)
-        _feedPosts.value = oldPosts
+        _screenState.value = oldPosts
     }
 }
 
--- app/src/main/java/com/sumin/vknewsclient/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/HomeScreen.kt
@@ -1,74 +1,99 @@
 package com.sumin.vknewsclient.ui.theme
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.DismissDirection
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.sumin.vknewsclient.MainViewModel
-import com.sumin.vknewsclient.domain.PostComment
+import com.sumin.vknewsclient.domain.FeedPost
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     viewModel: MainViewModel,
     paddingValues: PaddingValues,
 ) {
-    val feedPosts = viewModel.feedPosts.observeAsState(listOf())
+    val screenState = viewModel.screenState.observeAsState(HomeScreenState.Initial)
 
-    if (feedPosts.value.isNotEmpty()) {
-        val comments = mutableListOf<PostComment>().apply {
-            repeat(20) {
-                add(
-                    PostComment(id = it)
+    when (val currentState = screenState.value) {
+        is HomeScreenState.Posts -> {
+            FeedPosts(
+                viewModel = viewModel,
+                paddingValues = paddingValues,
+                posts = currentState.posts
+            )
+        }
+        is HomeScreenState.Comments -> {
+            CommentsScreen(feedPost = currentState.feedPost, comments = currentState.comments)
+        }
+        HomeScreenState.Initial -> {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
+@Composable
+private fun FeedPosts(
+    viewModel: MainViewModel,
+    paddingValues: PaddingValues,
+    posts: List<FeedPost>
+) {
+    LazyColumn(
+        modifier = Modifier.padding(paddingValues),
+        contentPadding = PaddingValues(
+            top = 16.dp,
+            start = 8.dp,
+            end = 8.dp,
+            bottom = 72.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(
+            items = posts,
+            key = { it.id }
+        ) { feedPost ->
+            val dismissState = rememberDismissState()
+            if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                viewModel.remove(feedPost)
+            }
+
+            SwipeToDismiss(
+                modifier = Modifier.animateItemPlacement(),
+                state = dismissState,
+                background = {},
+                directions = setOf(DismissDirection.EndToStart)
+            ) {
+                PostCard(
+                    feedPost = feedPost,
+                    onViewsClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                    onShareClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                    onCommentClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                    onLikeClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
                 )
             }
         }
-        CommentsScreen(feedPost = feedPosts.value.get(0), comments = comments)
     }
-
-
-//    LazyColumn(
-//        modifier = Modifier.padding(paddingValues),
-//        contentPadding = PaddingValues(
-//            top = 16.dp,
-//            start = 8.dp,
-//            end = 8.dp,
-//            bottom = 72.dp
-//        ),
-//        verticalArrangement = Arrangement.spacedBy(8.dp)
-//    ) {
-//        items(
-//            items = feedPosts.value,
-//            key = { it.id }
-//        ) { feedPost ->
-//            val dismissState = rememberDismissState()
-//            if (dismissState.isDismissed(DismissDirection.EndToStart)) {
-//                viewModel.remove(feedPost)
-//            }
-//
-//            SwipeToDismiss(
-//                modifier = Modifier.animateItemPlacement(),
-//                state = dismissState,
-//                background = {},
-//                directions = setOf(DismissDirection.EndToStart)
-//            ) {
-//                PostCard(
-//                    feedPost = feedPost,
-//                    onViewsClickListener = { statisticItem ->
-//                        viewModel.updateCount(feedPost, statisticItem)
-//                    },
-//                    onShareClickListener = { statisticItem ->
-//                        viewModel.updateCount(feedPost, statisticItem)
-//                    },
-//                    onCommentClickListener = { statisticItem ->
-//                        viewModel.updateCount(feedPost, statisticItem)
-//                    },
-//                    onLikeClickListener = { statisticItem ->
-//                        viewModel.updateCount(feedPost, statisticItem)
-//                    },
-//                )
-//            }
-//        }
-//    }
 }
+
+
+
+
+
