b3b02dd2af4aa2a26095534cfa0be1b9506ef7ae
a.sumin <sumin93@mail.ru>
2022-11-21 19:00:35 +0300

    Test ActivityResultApi

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.sumin.vknewsclient.ui.theme.ActivityResultTest
 import com.sumin.vknewsclient.ui.theme.MainScreen
 import com.sumin.vknewsclient.ui.theme.VkNewsClientTheme
 
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                MainScreen()
+                ActivityResultTest()
             }
         }
     }
---
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/ActivityResultTest.kt
@@ -0,0 +1,54 @@
+package com.sumin.vknewsclient.ui.theme
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun ActivityResultTest() {
+    var imageUri by remember {
+        mutableStateOf(Uri.EMPTY)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = {
+            imageUri = it
+        }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        AsyncImage(
+            modifier = Modifier.weight(1f),
+            model = imageUri,
+            contentDescription = null
+        )
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Blue,
+                contentColor = Color.White
+            ),
+            onClick = {
+                launcher.launch("image/*")
+            }
+        ) {
+            Text(text = "Get image")
+        }
+    }
+}
