07cdff458989dd50140e641ce21c79960f330281
sumin93 <suminfx@mail.ru>
2022-11-24 15:29:17 +0300

    Add VK auth - not working variant

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.sumin.vknewsclient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.sumin.vknewsclient.ui.theme.ActivityResultTest
 import com.sumin.vknewsclient.ui.theme.MainScreen
 import com.sumin.vknewsclient.ui.theme.VkNewsClientTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
 
@@ -14,7 +17,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                ActivityResultTest()
+                val launcher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract()
+                ) {
+                    when (it) {
+                        is VKAuthenticationResult.Success -> {
+                            Log.d("MainActivity", "Success auth")
+                        }
+                        is VKAuthenticationResult.Failed -> {
+                            Log.d("MainActivity", "Failed auth")
+                        }
+                    }
+                }
+                launcher.launch(listOf(VKScope.WALL))
+                MainScreen()
             }
         }
     }
